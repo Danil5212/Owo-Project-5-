0ff5de26c2e2f3ef6be714fc09a085526a67786c
Danil Malyshev <danil70malyshev@mail.ru~Function Key 6git config --global user.email danil70malyshev@gmail.ru>
2024-12-10 20:05:40 +0300

    <комментарий>

---
+++ Desktop/проект/Фома заветная/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Фома_заветная
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double one = double.Parse(textBox1.Text);
+                double two = double.Parse(textBox2.Text);
+                double sum = one / two;
+                if (two == 0)
+                {
+                    labelResult.Text = "На 0 делить нельзя.";
+                }
+                else
+                {
+                    labelResult.Text = sum.ToString();
+                }
+            }
+            catch (FormatException) 
+            {
+                labelResult.Text = "Ошибка:Введите числа.";
+            }
+            catch (Exception ex)
+            {
+                labelResult.Text = "Ошибка: " + ex.Message;
+            }
+        }
+    }
+}
